bce2c0cc353fa17705fba083d206dba56c9147b3
Rico Tyrell <teh_programerer@interactivelaboratories.com>
2017-01-15 01:15:27 +0000

    Added memory output debug information

--- Source/Source/Main.c
+++ Source/Source/Main.c
@@ -31,20 +31,73 @@ void main( void )
 		goto MainCleanup;
 	}
 
+	if( ( pGraphicsMemory = syMalloc( GraphicsMemorySize ) ) == NULL )
+	{
+		LOG_Debug( "Could not allocate graphics memory of size: %ld",
+			GraphicsMemorySize );
+
+		goto MainCleanup;
+	}
+
+	if( ( pAudioMemory = syMalloc( AudioMemorySize ) ) == NULL )
+	{
+		LOG_Debug( "Could not allocate audio memory of size: 5ld",
+			AudioMemorySize );
+
+		goto MainCleanup;
+	}
+
 	if( MEM_InitialiseMemoryBlock( &SystemMemoryBlock, pSystemMemory,
 		SystemMemorySize, 32, "System memory" ) != MEM_OK )
 	{
-		LOG_Debug( "Failed to initialise the system memory block fo size: %ld",
+		LOG_Debug( "Failed to initialise the system memory block of size: %ld",
 			SystemMemorySize );
 
 		goto MainCleanup;
 	}
 
-	/*while( 1 )
+	if( MEM_InitialiseMemoryBlock( &GraphicsMemoryBlock, pGraphicsMemory,
+		GraphicsMemorySize, 32, "Graphics memory" ) != MEM_OK )
 	{
-	}*/
+		LOG_Debug( "Failed to initialise the graphics memory block of size: "
+			"%ld", GraphicsMemorySize );
+
+		goto MainCleanup;
+	}
+
+	if( MEM_InitialiseMemoryBlock( &AudioMemoryBlock, pAudioMemory,
+		AudioMemorySize, 32, "Audio memory" ) != MEM_OK )
+	{
+		LOG_Debug( "Failed to initialise the audio memory block of size %ld",
+			AudioMemorySize );
+
+		goto MainCleanup;
+	}
 
 MainCleanup:
+	
+	if( pAudioMemory != NULL )
+	{
+		MEM_GarbageCollectMemoryBlock( &AudioMemoryBlock );
+		MEM_ListMemoryBlocks( &AudioMemoryBlock );
+		syFree( pAudioMemory );
+	}
+
+	if( pGraphicsMemory != NULL )
+	{
+		MEM_GarbageCollectMemoryBlock( &GraphicsMemoryBlock );
+		MEM_ListMemoryBlocks( &GraphicsMemoryBlock );
+		syFree( pGraphicsMemory );
+	}
+
+	if( pSystemMemory != NULL )
+	{
+		MEM_GarbageCollectMemoryBlock( &SystemMemoryBlock );
+		MEM_ListMemoryBlocks( &SystemMemoryBlock );
+		syFree( pSystemMemory );
+	}
+
+	LOG_Terminate( );
 	HW_Terminate( );
 	HW_Reboot( );
 }
